e82d45b718af434fee6182d3695a475a4eadbf16
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2020-03-13 01:42:39 +0300

    setup ESLint with Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const b: {[index: string]: number} = {
-    value: 4
-}
+const b: { [index: string]: number } = {
+  value: 4,
+};
 
-const c = (a: number) => a**2;
+const c = (a: number) => a ** 2;
 
 console.log(c(b.value));
