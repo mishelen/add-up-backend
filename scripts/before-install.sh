a06afdfd8b4091ead61a1fddfd30c0839298bea7
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2020-03-30 04:15:14 +0300

    move installation to home

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install curl -y
+cd /home/ec2-user/addup-backend
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
