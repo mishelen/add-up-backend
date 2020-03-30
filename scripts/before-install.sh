8b1849bf2b59dfce12042d575fcac353c7c9413c
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2020-03-30 06:45:03 +0300

    update scripts

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install curl -y
-adduser ec2-user
-sudo su - ec2-user
-cd /home/ec2-user/
+apt-get update
+apt-get install curl -y
+cd ~
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="/home/ec2-user/.nvm"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 command -v nvm # Verify that nvm has been installed
 nvm install 12
