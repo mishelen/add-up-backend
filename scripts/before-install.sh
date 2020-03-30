5d519e00d18eaa8c1faa4099681b5d4093da7ba8
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2020-03-30 04:58:44 +0300

    fix nvm dir and run as ec2 user

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/
 sudo apt-get update
 sudo apt-get install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 command -v nvm # Verify that nvm has been installed
 nvm install 12
